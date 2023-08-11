15ceddabea9b2fa995fb4257e6e24ea9e666eb81
miguel <juanmiguel21salonga@gmail.com>
2023-08-11 10:33:22 +0800

    initial commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  constructor(public photoService:PhotoService) {}
 
-  constructor() {}
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
 
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
 }
